8cbc05e797e83225c5de1d0dc6a0f5c1764e7867
Carvalho Estéban <esteban.carvalho@grenoble-inp.org>
2019-10-21 23:18:58 +0200

    Improvement
    - Start to clean code
    - Correction of some issues
    - Creation of subfolder

--- Gradient_Descent/Gradient_Descent_interactive_examples.m
+++ Gradient_Descent/Gradient_Descent_interactive_examples.m
@@ -21,7 +21,7 @@ function varargout = Gradient_Descent_interactive_examples(varargin)
 
 % Edit the above text to modify the response to help Gradient_Descent_interactive_examples
 
-% Last Modified by GUIDE v2.5 10-Oct-2019 21:27:26
+% Last Modified by GUIDE v2.5 19-Oct-2019 14:45:04
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -51,30 +51,63 @@ function Gradient_Descent_interactive_examples_OpeningFcn(hObject, eventdata, ha
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to Gradient_Descent_interactive_examples (see VARARGIN)
 
+% Populate axe
+try
+    logo = imread('./Pictures/2D_logo.png');
+    image(handles.axes2,logo)
+catch
+    disp('Pictures not found.')
+end
+
+axis(handles.axes2,'off')
+axis(handles.axes2,'image')
+
 % Choose default command line output for Gradient_Descent_interactive_examples
 handles.output = hObject;
 handles.affichage_value = 0 ;
 
+% Save x, y, X, Z for each function
+x1 = -20:0.1:20;
+y1 = -5:0.1:5;
+[X1,Y1] = meshgrid(x1,y1);
+Z1 = quadratic_plot(X1,Y1);
+
+handles.affichage_axes_values.X1 = X1;
+handles.affichage_axes_values.Y1 = Y1;
+handles.affichage_axes_values.Z1 = Z1;
+
+x2 = -2:0.01:2;
+y2 = -1:0.01:3;
+[X2,Y2] = meshgrid(x2,y2);
+Z2 = rosenbrock_plot(X2,Y2);
+handles.affichage_axes_values.X2 = X2;
+handles.affichage_axes_values.Y2 = Y2;
+handles.affichage_axes_values.Z2 = Z2;
+
+handles.Z_limits.ZL1 = [min(min(handles.affichage_axes_values.Z1)) max(max(handles.affichage_axes_values.Z1))];
+handles.Z_limits.ZL2 = [min(min(handles.affichage_axes_values.Z2)) max(max(handles.affichage_axes_values.Z2))];
+
 % Plot figure at GUI opening
-x = -20:0.1:20;
-y = -5:0.1:5;
-[X,Y] = meshgrid(x,y);
-Z = quadratic_plot(X,Y);
+
 if ~handles.affichage_value
     % 2D display
     hold on
-    contour(X,Y,Z)
+    contour(handles.affichage_axes_values.X1,handles.affichage_axes_values.Y1,...
+            handles.affichage_axes_values.Z1,'Parent', handles.axes1,'LevelList',[0.6 5 20 49.64 95.37 147.7 197.5 256.6 302.6 352 404.5]);
     
     % Get starting point
-    x0 = [str2double(handles.x01_value.String);str2double(handles.x02_value.String)];
+    x0 = [str2double(get(handles.x01_value,'String'));str2double(get(handles.x02_value,'String'))];
     plot(x0(1),x0(2),'r+','MarkerSize',10);
     plot(handles.axes1,0,0,'k+','MarkerSize',10);
 else
+    % On entre jamais ici
     % 3D display
-    mesh(X,Y,Z)
+    mesh(X1,Y1,Z1)
     view(20,50)
+  
 end
 box(handles.axes1,'on')
+
 % Update handles structure
 guidata(hObject, handles);
 
@@ -108,16 +141,16 @@ handles = guidata(hObject);
 alpha = get(hObject,'Value');
 
 % Display rounded value "with valeur_alpha_txt"
-handles.valeur_alpha_txt.String = num2str(round(alpha,4));
+set(handles.valeur_alpha_txt,'String',num2str(round(alpha,4)));
 
 % Get starting point
-x0 = [str2double(handles.x01_value.String);str2double(handles.x02_value.String)];
+x0 = [str2double(get(handles.x01_value,'String'));str2double(get(handles.x02_value,'String'))];
 
 % Check user choice Quadratic / Rosenbrock and create function to be used
 % for gradient descent
-if handles.checkbox1.Value
+if get(handles.checkbox1,'Value')
     obj_function = @(x) quadratic_function(x);
-elseif handles.checkbox2.Value
+elseif get(handles.checkbox2,'Value')
     obj_function = @(x) rosenbrock_function(x);
 end
 
@@ -138,58 +171,53 @@ else
     hold(handles.axes1,'on')
     plot3(handles.axes1,x0(1),x0(2),f_values(1),'r+','MarkerSize',10);
     view(handles.axes1,15,50)
+    grid(handles.axes1,'on')
 end
 
 
 % Plot contour according to user choice
-if handles.checkbox1.Value
-    x = -20:0.1:20;
-    y = -5:0.1:5;
-    [X,Y] = meshgrid(x,y);
-    Z = quadratic_plot(X,Y);
+if get(handles.checkbox1,'Value')
     if ~handles.affichage_value
-        contour(X,Y,Z,'Parent', handles.axes1)
+        contour(handles.affichage_axes_values.X1,handles.affichage_axes_values.Y1,...
+            handles.affichage_axes_values.Z1,'Parent', handles.axes1,'LevelList',[0.6 5 20 49.64 95.37 147.7 197.5 256.6 302.6 352 404.5])
         plot(handles.axes1,0,0,'k+','MarkerSize',10);
         plot(handles.axes1,sol(1),sol(2),'ko','MarkerSize',10);
+        grid(handles.axes1,'off')
     else
-        mesh(X,Y,Z,'Parent', handles.axes1)
+        mesh(handles.affichage_axes_values.X1,handles.affichage_axes_values.Y1,...
+            handles.affichage_axes_values.Z1,'Parent', handles.axes1)
         plot3(handles.axes1,0,0,0,'k+','MarkerSize',15);
         plot3(handles.axes1,sol(1),sol(2),f_values(end),'ko','MarkerSize',10);
         view(handles.axes1,15,50)
-        zlim(handles.axes1,[min(min(Z)) max(max(Z))]);
+        zlim(handles.axes1,handles.Z_limits.ZL1);
+        grid(handles.axes1,'on')
     end
     xlim(handles.axes1,[-20,20]);
     ylim(handles.axes1,[-5,5]);
     hold(handles.axes1,'off')
     
-elseif handles.checkbox2.Value
-    x = -2:0.01:2;
-    y = -1:0.01:3;
-    [X,Y] = meshgrid(x,y);
-    Z = rosenbrock_plot(X,Y);
+elseif get(handles.checkbox2,'Value')
     if ~handles.affichage_value
-        contour(X,Y,Z,'Parent', handles.axes1,'LevelList',[0.1,3,5,10,20,50,200,450,1500])
+        contour(handles.affichage_axes_values.X2,handles.affichage_axes_values.Y2,...
+            handles.affichage_axes_values.Z2,'Parent', handles.axes1,'LevelList',[0.1,3,5,10,20,50,200,450,1500])
         plot(handles.axes1,sol(1),sol(2),'ko','MarkerSize',10);
         plot(handles.axes1,1,1,'k+','MarkerSize',10);
-        
+        grid(handles.axes1,'off')
     else
-        mesh(X,Y,Z,'Parent', handles.axes1)
+        mesh(handles.affichage_axes_values.X2,handles.affichage_axes_values.Y2,...
+            handles.affichage_axes_values.Z2,'Parent', handles.axes1)
         plot3(handles.axes1,1,1,0,'k+','MarkerSize',15);
         plot3(handles.axes1,sol(1),sol(2),f_values(end),'ko','MarkerSize',10);
-        view(handles.axes1,15,50) 
-        zlim(handles.axes1,[min(min(Z)) max(max(Z))]);
+        view(handles.axes1,15,50)
+        zlim(handles.axes1,handles.Z_limits.ZL2);
+        grid(handles.axes1,'on')
     end
-
     xlim(handles.axes1,[-2,2]);
     ylim(handles.axes1,[-1,3]);
     hold(handles.axes1,'off')
 end
 box(handles.axes1,'on')
 guidata(hObject, handles);
-
-
-
-
 
 % --- Executes during object creation, after setting all properties.
 function slider1_CreateFcn(hObject, eventdata, handles)
@@ -208,120 +236,125 @@ end
 
 % --- Executes on button press in checkbox1.
 function checkbox1_Callback(hObject, eventdata, handles)
+handles.output = hObject;
 % hObject    handle to checkbox1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Untick other choices
-handles.checkbox2.Value = 0;
+set(handles.checkbox2,'Value',0);
 
-if handles.checkbox2.Value == 0 && handles.checkbox1.Value == 0
-    handles.checkbox1.Value = 1;
+if (get(handles.checkbox2,'Value') == 0) && (get(handles.checkbox1,'Value') == 0)
+    set(handles.checkbox1,'Value', 1);
 end
 
 % Change slider min / max values and texts
-handles.step_min_val.String = 0;
-handles.step_max_val.String = 0.5;
+set(handles.step_min_val,'String',0);
+set(handles.step_max_val,'String',0.5);
 
-handles.slider1.Min = 0;
-handles.slider1.Max = 0.5;
+set(handles.slider1,'Min', 0);
+set(handles.slider1,'Max', 0.5);
 
 % Put slider to 0
-handles.slider1.Value = 0;
+set(handles.slider1,'Value',0);
 
 % Display function expression
-handles.function_text.String = "f(x,y) = x² + 2·y²";
+set(handles.function_text,'String', 'f(x,y) = x² + 2·y²');
 
 % Get alpha value
-alpha = handles.slider1.Value;
-handles.valeur_alpha_txt.String = num2str(round(alpha,5));
+alpha = get(handles.slider1,'Value');
+set(handles.valeur_alpha_txt,'String',num2str(round(alpha,5)));
 
 % Clean and replot everything
 cla(handles.axes1)
 
 hold(handles.axes1,'on')
-x = -20:0.1:20;
-y = -5:0.1:5;
-[X,Y] = meshgrid(x,y);
-Z = quadratic_plot(X,Y);
+
 if ~handles.affichage_value
-    contour(X,Y,Z,'Parent', handles.axes1)
+    contour(handles.affichage_axes_values.X1,handles.affichage_axes_values.Y1,handles.affichage_axes_values.Z1,...
+        'Parent', handles.axes1,'LevelList',[0.6 5 20 49.64 95.37 147.7 197.5 256.6 302.6 352 404.5]);
     plot(handles.axes1,0,0,'k+','MarkerSize',10);
     view(handles.axes1,2)
+    grid(handles.axes1,'off')
 else
-    mesh(X,Y,Z,'Parent', handles.axes1)
+    mesh(handles.affichage_axes_values.X1,handles.affichage_axes_values.Y1,handles.affichage_axes_values.Z1,'Parent', handles.axes1)
     plot3(handles.axes1,0,0,0,'k+','MarkerSize',15);
     view(handles.axes1,15,50)
-    zlim(handles.axes1,[min(min(Z)) max(max(Z))]);
+    zlim(handles.axes1,handles.Z_limits.ZL1);
+    grid(handles.axes1,'on')
 end
 xlim(handles.axes1,[-20,20]);
 ylim(handles.axes1,[-5,5]);
 
 % Set new starting point
-handles.x01_value.String ='-4.5';
-handles.x02_value.String ='4.5';
+set(handles.x01_value,'String','-4.5');
+set(handles.x02_value,'String','4.5');
 
 % Plot this new point
 if ~handles.affichage_value
     plot(handles.axes1,-4.5,4.5,'r+','MarkerSize',10);
+    grid(handles.axes1,'off')
 else
     fx0 = quadratic_function([-4.5;4.5]);
     plot3(handles.axes1,-4.5,4.5,fx0,'r+','MarkerSize',15);
+    grid(handles.axes1,'on')
 end
 box(handles.axes1,'on')
 guidata(hObject, handles);
 
-
 % --- Executes on button press in checkbox2.
 function checkbox2_Callback(hObject, eventdata, handles)
+handles.output = hObject;
 % hObject    handle to checkbox2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Untick other choices
-handles.checkbox1.Value = 0;
+set(handles.checkbox1,'Value',0);
 
-if handles.checkbox2.Value == 0 && handles.checkbox1.Value == 0
-    handles.checkbox2.Value = 1;
+if (get(handles.checkbox2,'Value') == 0) && (get(handles.checkbox1,'Value') == 0)
+    set(handles.checkbox2,'Value',1);
 end
 
 % Change slider min / max values and texts
-handles.step_min_val.String = 0;
-handles.step_max_val.String = 0.005;
-handles.slider1.Min = 0;
-handles.slider1.Max = 0.005;
+set(handles.step_min_val,'String','0');
+set(handles.step_max_val,'String','0.005');
+
+set(handles.slider1,'Min',0);
+set(handles.slider1,'Max', 0.005);
 
 % Display function expression
-handles.slider1.Value = 0;
-handles.function_text.String = "f(x,y) = (1-x)² + 100·(y-x²)²";
+set(handles.slider1,'Value',0);
+set(handles.function_text,'String','f(x,y) = (1-x)² + 100·(y-x²)²');
 
 % Get alpha value
-alpha = handles.slider1.Value;
-handles.valeur_alpha_txt.String = num2str(round(alpha,5));
+alpha = get(handles.slider1,'Value');
+set(handles.valeur_alpha_txt,'String', num2str(round(alpha,5)));
 
 % Clean and replot everything
 cla(handles.axes1)
 hold(handles.axes1,'on')
-x = -2:0.01:2;
-y = -1:0.01:3;
-[X,Y] = meshgrid(x,y);
-Z = rosenbrock_plot(X,Y);
+
 if ~handles.affichage_value
-    contour(X,Y,Z,'Parent', handles.axes1,'LevelList',[0.1,3,5,10,20,50,200,450,1500]);
+    contour(handles.affichage_axes_values.X2,handles.affichage_axes_values.Y2,...
+        handles.affichage_axes_values.Z2,'Parent', handles.axes1,'LevelList',[0.1,3,5,10,20,50,200,450,1500]);
     plot(handles.axes1,1,1,'k+','MarkerSize',10);
-     view(handles.axes1,2)
+    view(handles.axes1,2)
+    grid(handles.axes1,'off')
 else
-    mesh(X,Y,Z,'Parent', handles.axes1)
+    mesh(handles.affichage_axes_values.X2,handles.affichage_axes_values.Y2,...
+        handles.affichage_axes_values.Z2,'Parent', handles.axes1)
     plot3(handles.axes1,1,1,0,'k+','MarkerSize',15);
     view(handles.axes1,15,50)
-    zlim(handles.axes1,[min(min(Z)) max(max(Z))]);
+    zlim(handles.axes1,handles.Z_limits.ZL2);
+    grid(handles.axes1,'on')
 end
 xlim(handles.axes1,[-2,2]);
 ylim(handles.axes1,[-1,3]);
 
 % Set new starting point
-handles.x01_value.String ='-1.5';
-handles.x02_value.String ='1.5';
+set(handles.x01_value,'String','-1.5');
+set(handles.x02_value,'String','1.5');
 
 % Plot this new point
 if ~handles.affichage_value
@@ -385,7 +418,7 @@ function x01_value_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Set min and max value we can enter
-if handles.checkbox1.Value
+if get(handles.checkbox1,'Value')
     lim_val_max = 20;
     lim_val_min = -lim_val_max;
 elseif handles.checkbox2.Value
@@ -397,9 +430,9 @@ end
 value = round(str2double(get(hObject,'String')),1);
 
 % Check if value is a number
-if(isempty(value))
+if isempty(value) || isnan(value)
     value = 1;
-    hObject.String = '1';
+    set(hObject,'String','1');
 end
 
 % Check if value respects the limits
@@ -409,9 +442,8 @@ elseif value >lim_val_max
     value = lim_val_max;
 end
 
-hObject.String = num2str(value);
-handles.Ti_value.Value = value;
-handles.slider1.Value = 0;
+set(hObject,'String',num2str(value));
+set(handles.slider1,'Value',0);
 slider1_Callback(hObject, eventdata, handles)
 guidata(hObject, handles);
 
@@ -436,7 +468,7 @@ function x02_value_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Set min and max value we can enter
-if handles.checkbox1.Value
+if get(handles.checkbox1,'Value')
     lim_val_max = 5;
     lim_val_min = -lim_val_max;
 elseif handles.checkbox2.Value
@@ -448,9 +480,9 @@ end
 value = round(str2double(get(hObject,'String')),1);
 
 % Check if value is a number
-if(isempty(value))
+if isempty(value) || isnan(value)
     value = 1;
-    hObject.String = '1';
+    set(hObject,'String','1');
 end
 
 % Check if value respects the limits
@@ -460,9 +492,8 @@ elseif value >lim_val_max
     value = lim_val_max;
 end
 
-hObject.String = num2str(value);
-handles.Ti_value.Value = value;
-handles.slider1.Value = 0;
+set(hObject,'String',num2str(value));
+set(handles.slider1,'Value', 0);
 slider1_Callback(hObject, eventdata, handles)
 guidata(hObject, handles);
 
@@ -494,10 +525,35 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
 handles.affichage_value = ~handles.affichage_value;
+try
+    if handles.affichage_value
+        logo = imread('./Pictures/3D_logo.png');
+    else
+        logo = imread('./Pictures/2D_logo.png');
+    end
+    image(handles.axes2,logo)
+catch
+    disp('Pictures not found.')
+end
+axis(handles.axes2,'off')
+axis(handles.axes2,'image')
+    
 guidata(hObject, handles);
-if handles.checkbox1.Value
+
+if get(handles.checkbox1,'Value')
     checkbox1_Callback(hObject, eventdata, handles)
 else
     checkbox2_Callback(hObject, eventdata, handles)
 end
+guidata(hObject, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function axes2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes2
